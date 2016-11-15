27d57829f11b2ed13d8f60d7624c616d67c64c07
Brandon Ripley <Hyiero@Brandons-MacBook-Pro.local>
2016-11-14 21:51:39 -0500

    Working on getting the source maps setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 var browserify = require('browserify');
 var gulp = require('gulp');
 var source = require('vinyl-source-stream');
+var sourceMaps = require('gulp-sourcemaps');
+var buffer = require('vinyl-buffer');
 
 gulp.task('default', ['clean','browserify']);
-gulp.task('browserify',browserify);
+gulp.task('browserify',bundleApp);
 gulp.task('clean',clean);
 
-function browserify(){
+function bundleApp(){
 
     var sourceFile = './app/app.module.js';
 
@@ -14,6 +16,11 @@ function browserify(){
         .bundle()
         //Pass desired output filename to vinyl-source-stream
         .pipe(source('bundle.js'))
+        .pipe(buffer())
+        //Initialize sourcemaps for anything under this
+        .pipe(sourceMaps.init({loadMaps: true}))
+        //Write sourcemaps out to the build folder
+        .pipe(sourceMaps.write('./'))
         //Start piping stream to tasks!
         .pipe(gulp.dest('./build'));
 }
